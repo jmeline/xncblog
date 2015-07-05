89c12e445dca6c4a36b875841f4da2f048755f6c
Jacob Meline <jacob.meline@gmail.com>
2015-07-04 21:33:53 -0700

    Adding a simple sidebar to website

--- xnc/routes/index.js
+++ xnc/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Jacob' });
+  res.render('index', {
+      title: 'Express',
+      name: 'Jacob'
+  });
 });
 
 module.exports = router;
