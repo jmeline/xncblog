c0ba61f3d6512d1a17f87222237def1d9c59289b
Jacob Meline <jacob.meline@gmail.com>
2015-08-13 07:56:48 -0700

    Switching from Jade to EJS view engine. Beginning to include Angularjs

--- xnc/routes/index.js
+++ xnc/routes/index.js
@@ -3,19 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-      title: 'Express',
-      name: 'Jacob'
-  });
+    res.render("./layout");
 });
 
-router.get('/blog', function(req, res, next){
-    res.render('./sidebar_links/blog', {
-    });
-});
-router.get('/programming', function(req, res, next){
-    res.render('./sidebar_links/programming', {
-    });
-});
+// router.get('/blog', function(req, res, next){
+//     res.render('./sidebar_links/blog', {
+//     });
+// });
+// router.get('/programming', function(req, res, next){
+//     res.render('./sidebar_links/programming', {
+//     });
+// });
 
 module.exports = router;
