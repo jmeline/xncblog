9bc80fdf75598b6799caac06c33be01a9f207252
Jacob Meline <jacob.meline@gmail.com>
2015-11-19 10:35:36 -0700

    Updated dependancies.sh to create package.json if it does not exist

--- dependancies.sh
+++ dependancies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
+if [ ! -f package.json ]; then
+    npm init
+fi
 # this is the npm install commands I used to create this environment
-sudo npm install --save browserify reactify vinyl-source-stream
-sudo npm install --save bootstrap jquery gulp-concat
-sudo npm install --save gulp-eslint
-sudo npm install --save react react-router flux history@^1.12.0
-sudo npm install --save lodash
+sudo npm install --save browserify reactify vinyl-source-stream bootstrap jquery gulp gulp-open gulp-concat gulp-connect gulp-eslint react react-router flux history@^1.12.0 lodash
