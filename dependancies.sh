023ad2c0136533ef2e1094abda49f5b821e38053
Jacob Meline <jacob.meline@gmail.com>
2015-11-19 10:08:59 -0700

    Switching to reactjs. Learning reactjs and hopefully can transform blog into reactjs

---
+++ dependancies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this is the npm install commands I used to create this environment
+sudo npm install --save browserify reactify vinyl-source-stream
+sudo npm install --save bootstrap jquery gulp-concat
+sudo npm install --save gulp-eslint
+sudo npm install --save react react-router flux history@^1.12.0
+sudo npm install --save lodash
