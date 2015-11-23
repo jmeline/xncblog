93aec553e511018f13b24fd09fd46ea8ce6804c5
Jacob Meline <jacob.meline@gmail.com>
2015-11-23 10:37:46 -0700

    Adding react-router links to page instead of default <a hrefs>

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,13 +1,18 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({
     render: function(){
         return (
             <div>
-                <h1>Pluralsight Administration</h1>
-                <p>React, React Router, and Flux for ultra-responsive we apps. </p>
+                <h1>Blog</h1>
+                <p>
+                    <h4>React, React Router, and Flux for ultra-responsive we apps. </h4>
+                </p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
             </div>
         );
     }
