e4e4236e8ed299f14be1ca51ed5617054af7892e
Jacob Meline <jacob.meline@gmail.com>
2015-12-07 08:05:19 -0700

    Starting final challenge

---
+++ src/components/courses/coursePage.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var React = require('react');
+
+var Courses = React.createClass({
+    render: function(){
+
+        return (
+            <div>
+                <h1>Courses</h1>
+                Interesting STuff will be here
+            </div>
+        );
+    }
+});
+
+
+module.exports = Courses;
--- src/routes.js
+++ src/routes.js
@@ -17,7 +17,9 @@ var routes = (
         <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage')} />
         <Route name="manageAuthor" path="author/:id" handler={require('./components/authors/manageAuthorPage')} />
         <Route name="about" handler={require('./components/about/aboutpage')} />
+        <Route name="courses" handler={require('./components/courses/coursePage')} />
         <Route name="tutorials" handler={require('./components/tutorials/tutorialspage')} />
+
         <Redirect from="about-us" to="about" />
         <Redirect from="awthurs" to="authors" />
         <Redirect from="about/*" to="about" />
